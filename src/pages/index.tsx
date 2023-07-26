0f2d4b09e1a7741369c54caacd3e04b8d705b101
bkim4472 <brandonkim4725@gmail.com>
2023-07-26 09:05:42 -0700

    Adding Input Boxes For All Person Fields

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,21 +46,108 @@ const PersonItem: React.FC<{ person: Person }> = ({ person }) => (
 
 const AddPersonInput = () => {
   const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [gender, setGender] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [street1, setStreet1] = useState("");
+  const [street2, setStreet2] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  //const is similar but better than var, constant value cannot change
+  //let can be changed throughout
+  //State
+  
 
   return (
     <form
       onSubmit={async e => {
         e.preventDefault();
         createPerson(firstName);
+
+
+        //resetting each textbox back to placeholder
         setFirstname("");
+        setLastname("");
+        setGender("");
+        setBirthdate("");
+        setStreet1("");
+        setStreet2("");
+        setCity("");
+        setState("");
+        setZip("");
+        setEmail("");
+        setPhoneNumber("");
       }}
       className={styles.addTodo}
     >
       <input
         className={styles.input}
-        placeholder="Tony"
+        placeholder="First name"
         value={firstName}
         onChange={e => setFirstname(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Last name"
+        value={lastName}
+        onChange={e => setLastname(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Gender"
+        value={gender}
+        onChange={e => setGender(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Birthdate"
+        value={birthdate}
+        onChange={e => setBirthdate(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Street1"
+        value={street1}
+        onChange={e => setStreet1(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Street2"
+        value={street2}
+        onChange={e => setStreet2(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="City"
+        value={city}
+        onChange={e => setCity(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="State"
+        value={state}
+        onChange={e => setState(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Zip"
+        value={zip}
+        onChange={e => setZip(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input
+        className={styles.input}
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChange={e => setPhoneNumber(e.target.value)}
       />
       <button className={styles.addButton}>Add</button>
     </form>
@@ -76,7 +163,7 @@ const Home: NextPage = () => {
       </Head>
 
       <header className={styles.header}>
-        <h1 className={styles.title}>Persons</h1>
+        <h1 className={styles.title}>People</h1>
         <h2 className={styles.desc}>
           NextJS app connected to Postgres using Prisma and hosted on{" "}
           <a href="https://railway.app">Railway</a>
