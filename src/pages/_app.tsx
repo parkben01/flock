40c0ee7576cc752b37d6afaadd8f4970ec8d9f9e
Ben Park <4203584+parkben01@users.noreply.github.com>
2023-08-30 19:04:47 -0700

    add auth, landing page, logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import { SessionProvider } from "next-auth/react"
+import Layout from '../components/layout'
 import '../styles/globals.css'
+import type { AppProps } from "next/app"
+import type { Session } from "next-auth"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  )
 }
-
-export default MyApp
